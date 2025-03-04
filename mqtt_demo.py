ef629b58addbf4a10c540ef90d8c61e754e22663
Andrew Leech <andrew@alelec.net>
2025-03-04 21:29:12 +1100

    Trial to use zcattacz/mqtt_as library.

--- mqtt_demo.py
+++ mqtt_demo.py
@@ -3,8 +3,9 @@ import asyncio
 import json
 import machine
 import network
+import rp2
 
-from mqtt_as import MQTTClient, config
+from mqtt_as import config, MQTT_base as MQTTClient
 from asyncpin import AsyncPinDb
 from broker import Broker
 
@@ -35,13 +36,18 @@ async def wait_for_host_connection():
         host_ip = "169.254.128.16"
     print(f"Connecting to host: {host_ip}")
     config['server'] = host_ip
+    config["client_id"] = network.hostname()
 
 
 async def monitor_hardware(client):
-    button = AsyncPinDb(machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN), machine.Pin.IRQ_RISING)
-    print(button)
+    # button = AsyncPinDb(machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN), machine.Pin.IRQ_RISING)
+    current = 0
     while True:
-        await button.wait_edge()
+        # await button.wait_edge()
+        while rp2.bootsel_button() == current:
+            await asyncio.sleep_ms(100)
+        current = rp2.bootsel_button()
+
         client.publish(
             "button_topic", 
             json.dumps({
@@ -57,7 +63,7 @@ async def handle_led(topic, msg):
 
 
 async def main():
-    try:
+    # try:
         client = None
         await wait_for_host_connection()
         
@@ -67,17 +73,27 @@ async def main():
         await client.connect()
 
         asyncio.create_task(_reconnect(client))
-        asyncio.create_task(_messages(client, broker))
+        # asyncio.create_task(_messages(client, broker))
         
         asyncio.create_task(monitor_hardware(client))
 
         broker.subscribe("led_topic", handle_led)
-        await client.subscribe('led_topic', 1)
+
+        client.set_cb_on_event("msg", _messages, udata=broker)
+
+        # while True:
+        #     await client.up.wait()
+        #     client.up.clear()
+        #     print("mqtt connected")
+        #     client.set_cb_on_event("msg", _messages)
+
+        #     await client.subscribe('led_topic', 1)
 
         # asyncio.create_task(aiorepl.task())
 
         # asyncio.get_event_loop().run_forever()
             
+        await client.up.wait()
         n = 0
         while True:
             await asyncio.sleep(5)
@@ -85,16 +101,17 @@ async def main():
             # If WiFi is down the following will pause for the duration.
             await client.publish('result_topic', '{}'.format(n), qos = 1)
             n += 1
-    finally:
-        if client:
-            client.close()  # Prevent LmacRxBlk:1 errors
+    # finally:
+    #     if client:
+    #         client.close()  # Prevent LmacRxBlk:1 errors
 
 
 
-async def _messages(client, broker):  # Respond to incoming messages
-    async for topic, msg, retained in client.queue:
-        print(topic.decode(), msg.decode(), retained)
-        broker.publish(topic.decode(), msg.decode())
+async def _messages(topic, msg, retained, udata):  # Respond to incoming messages
+    broker = udata
+    print(topic.decode(), msg.decode(), retained)
+    broker.publish(topic.decode(), msg.decode())
+
 
 async def _reconnect(client):  # Respond to connectivity being (re)established
     while True:
@@ -102,13 +119,15 @@ async def _reconnect(client):  # Respond to connectivity being (re)established
         client.up.clear()
         # renew subscriptions
         # await client.subscribe('degraves_topic', 1)  
+        print("Subscribing")
         await client.subscribe('led_topic', 1)
+        # await client.disconnect()
 
 
-config["queue_len"] = 1  # Use event interface with default queue size
-config["quick"] = 1  # Fast startup as we're not waiting for wifi
+# config["queue_len"] = 1  # Use event interface with default queue size
+# config["quick"] = 1  # Fast startup as we're not waiting for wifi
 
-MQTTClient.DEBUG = True  # Optional: print diagnostic messages
+# MQTTClient.DEBUG = True  # Optional: print diagnostic messages
 
 
 asyncio.run(main())
